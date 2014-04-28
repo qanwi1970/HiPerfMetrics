b6356e0da39e31af2dfeac9787ea74750a7a9a18
Bill DeLude <Bill.DeLude@RelayHealth.com>
2014-04-28 14:20:06 -0700

    Some fixes, and a nuget release

---
+++ src/HiPerfMetrics.Tests/Reports/DefaultReportTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using HiPerfMetrics.Reports;
+using NUnit.Framework;
+
+namespace HiPerfMetrics.Tests.Reports
+{
+    [TestFixture]
+    public class DefaultReportTest
+    {
+        [Test]
+        public void NormalTwoTaskReport()
+        {
+            var metric = new HiPerfMetric("NormalTwoTaskReport");
+            metric.Start("task 1");
+            Thread.Sleep(250);
+            metric.Stop();
+            metric.Start("task 2");
+            Thread.Sleep(500);
+            metric.Stop();
+
+            var report = new DefaultReport
+            {
+                Metric = metric
+            };
+            Debug.WriteLine(report.Report());
+        }
+    }
+}

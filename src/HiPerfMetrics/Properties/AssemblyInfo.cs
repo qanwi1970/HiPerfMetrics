bc54e2cd6ccefbfc6712e74573f040a0f4f186a3
qanwi1970 <qanwi1970@gmail.com>
2015-07-04 17:27:57 -0600

    publish to nuget as version 1.2.0

--- src/HiPerfMetrics/Properties/AssemblyInfo.cs
+++ src/HiPerfMetrics/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HiPerfMetrics")]
-[assembly: AssemblyCopyright("Copyright © Bill DeLude 2014")]
+[assembly: AssemblyCopyright("Copyright © Bill DeLude 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3.*")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0.*")]
